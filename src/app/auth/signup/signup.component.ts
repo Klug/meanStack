817bcd7f21be74d43cd35f0cfa5d41de2c50b764
PhilippKlug <klug@ca-digit.com>
2019-02-08 14:07:05 +0100

    changed some merge misstakes

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,8 @@
 import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
-<<<<<<< HEAD
-=======
+
 import {AuthService} from '../auth.service';
->>>>>>> dev
+
 
 @Component({
   templateUrl: './signup.component.html',
@@ -13,17 +12,13 @@ import {AuthService} from '../auth.service';
 export class SignupComponent {
   isLoading = false;
 
-<<<<<<< HEAD
-  onSignup(form: NgForm) {
-    console.log(form.value);
-=======
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService) { }
 
   onSignup(form: NgForm) {
     if (form.invalid) {
       return;
     }
     this.authService.createUser(form.value.email, form.value.password);
->>>>>>> dev
+
   }
 }
